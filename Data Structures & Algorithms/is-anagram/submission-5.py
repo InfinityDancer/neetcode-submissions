f47488928b7b6f8b4632b8440330256c95c2064f
InfinityDancer <66729493+InfinityDancer@users.noreply.github.com>
2026-05-19 18:41:44 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+
+        if len(s) != len(t):
+            return False
+
+        letter_map = {}
+
+        for letter in s:
+            letter_map[letter] = letter_map.get(letter, 0) + 1
+
+            # print(f'{letter_map =}')
+
+        for letter in t:
+            if letter not in letter_map:
+                return False
+            
+            letter_map[letter] -= 1
+
+            if letter_map[letter] < 0:
+                return False
+
+        return True
